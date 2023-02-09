e3b61e2b33283f9faef21ea5d3509e0196c7574e
Yuriy Grigoryev <allenvox@users.noreply.github.com>
2023-02-09 09:19:39 +0700

    Implement printing out the matrixes

--- main.cpp
+++ main.cpp
@@ -1,23 +1,45 @@
 #include <iostream>
 
+void printMatrix(float **matrix, int n)
+{
+    std::cout << "Your matrix now looks like this:" << std::endl;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n + 1; j++)
+        {
+            std::cout << matrix[i][j] << "\t";
+            if (j == n - 1)
+            {
+                std::cout << "\t|\t";
+            }
+        }
+        std::cout << std::endl;
+    }
+    std::cout << "\n";
+}
+
 int main()
 {
     int n;
     std::cout << "Enter the number of unknowns: ";
     std::cin >> n;
-    float matrix[n][n + 1];
+    float **matrix = (float **)malloc(n * sizeof(float *));
     float result[n];
 
     // matrix initialization
     std::cout << "Enter the elements of the matrix: " << std::endl;
     for (int i = 0; i < n; i++)
     {
+        matrix[i] = (float *)malloc((n + 1) * sizeof(float));
         for (int j = 0; j < n + 1; j++)
         {
             std::cout << "m[" << i << "][" << j << "] = ";
             std::cin >> matrix[i][j];
         }
     }
+    std::cout << "\n";
+
+    printMatrix(matrix, n);
 
     // making the upper triangular matrix
     for (int j = 0; j < n - 1; j++)
@@ -31,6 +53,8 @@ int main()
             }
         }
     }
+
+    printMatrix(matrix, n);
 
     // backwards substitution
     for (int i = n - 1; i >= 0; i--)
